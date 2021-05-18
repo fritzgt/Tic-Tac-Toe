abe4f6ad063c196e580a971e0a45208a004d039e
exoticproductions <contactexoticproductions@gamail.com>
2021-05-18 08:25:02 +0300

    Completed AI logic

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
                     ForEach(0..<9) { i in
                         ZStack{
                             Circle()
-                                .foregroundColor(.purple).opacity(0.5)
+                                .foregroundColor(.blue).opacity(0.5)
                                 .frame(width: geometry.size.width/3 - 15,
                                        height: geometry.size.width/3 - 15)
                             Image(systemName: moves[i]?.indicator ?? "")
@@ -57,8 +57,8 @@ struct ContentView: View {
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                 let computerPosition = determineComputerMovePosition(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
-                                isGameboardDisable = false
                                 
+                                isGameboardDisable = false
                                 //Check for win conditions
                                 if checkWinCondition(for: .computer, in: moves){
                                     alertItem =  AletContext.computerWin
@@ -69,6 +69,7 @@ struct ContentView: View {
                                     alertItem =  AletContext.draw
                                     return
                                 }
+                                
                             }
                             
                         }
@@ -88,7 +89,52 @@ struct ContentView: View {
         return moves.contains(where: {$0?.boardIndex == index})
     }
     
+   
     func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        //If AI can win, then win
+        //1.Posible win positions
+        let winPattern: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        //2.Remove nils from all positions and then filter to get only computer positions
+        let computerMoves = moves.compactMap { $0 }.filter{$0.player == .computer}
+        //3.Get all the indexes of the computer positions
+        let computerPositions = Set(computerMoves.map {$0.boardIndex})
+        //4.Iterate over the posible win positions
+        for pattern in winPattern {
+            //1.Check each patter and substract/remove the current computer position
+            let winPositions = pattern.subtracting(computerPositions)
+            //2.If is only one move needed to win
+            if winPositions.count == 1 {
+                //1.Check is the one position to win is available
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                //2.Take position if is available and return to prevent the rest of the code from running
+                if isAvailable { return winPositions.first!}
+            }
+        }
+        
+        //If AI can't win, then block(Similar to above but this time we check for human moves to block
+        
+        //1.Remove nils from all positions and then filter to get only computer positions
+        let humanMoves = moves.compactMap { $0 }.filter{$0.player == .human}
+        //2.Get all the indexes of the human positions
+        let humanPositions = Set(humanMoves.map {$0.boardIndex})
+        //3.Iterate over the posible win positions
+        for pattern in winPattern {
+            //1.Check each patter and substract/remove the current computer position
+            let winPositions = pattern.subtracting(humanPositions)
+            //2.If is only one move needed to win
+            if winPositions.count == 1 {
+                //1.Check is the one position to win is available
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                //2.Take position if is available and return to prevent the rest of the code from running
+                if isAvailable { return winPositions.first!}
+            }
+        }
+        
+        //If AI cant block, then take middle square
+        let isMiddlePositionAvailable = !isSquareOccupied(in: moves, forIndex: 4)
+        if isMiddlePositionAvailable { return 4 }
+        
+        //If AI can't take middle square, then random available square
         var movePosition = Int.random(in: 0..<9)
         
         //While square is occupied try another position
